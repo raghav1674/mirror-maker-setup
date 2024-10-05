b4f79f617b622db09716be67fb5d75bdc97052dc
raghav1674 <ragaws1674@gmail.com>
2024-10-05 19:05:40 +0530

    Updated cluster aliases for hbc and cpc connectors

--- scripts/configure-connectors.sh
+++ scripts/configure-connectors.sh
@@ -10,5 +10,12 @@ curl -X PUT -H "Content-Type: application/json" --data @configurations/mm2-cpc-s
 curl -X PUT -H "Content-Type: application/json" --data @configurations/mm2-hbc-sasl-auth.json ${CONNECT_HOST}/connectors/mm2-hbc/config
 
 
+# Enable TRACE logging for MirrorCheckpointConnector
+curl -s -X PUT -H "Content-Type:application/json" http://${CONNECT_HOST}/admin/loggers/org.apache.kafka.connect.mirror.MirrorCheckpointConnector \ -d '{"level": "TRACE"}' 
+
+# restart connectors
+curl -X POST ${CONNECT_HOST}/connectors/mm2-cpc/restart
+
+curl -X POST ${CONNECT_HOST}/connectors/mm2-hbc/restart
 
 
